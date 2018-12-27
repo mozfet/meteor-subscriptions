cbf04c8f8670f391faae012660c264c0b071a70a
Mozfet <wynand@expertbox.com>
2018-12-27 14:35:39 +0200

    first commit

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Extends mozfet:payments with subscription product management.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/mozfet/meteor-subscriptions.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
